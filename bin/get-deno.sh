a8650cbd584d31841ad7c88365a623b734a8df9a
casjay <git-admin@casjaysdev.com>
2022-10-12 20:11:18 -0400

    🗃️ Committing everything that changed 🗃️

--- bin/get-deno.sh
+++ bin/get-deno.sh
@@ -42,7 +42,7 @@ echo "grabbing deno $DENO_VERSION from $CHANNEL for $ARCH"
 if curl -q -LSsf -o "$TMP_FILE" "$URL" && [ -f "$TMP_FILE" ]; then
   mkdir -p "$TMP_DIR" && cd "$TMP_DIR" || exit 10
   unzip "$TMP_FILE"
-  mv -fv "$TMP_DIR/deno" "$BIN_FILE"
+  cp -Rf "$TMP_DIR/deno" "$BIN_FILE"
   ls -lA "$BIN_FILE" "$TMP_DIR/deno"
   chmod -Rf 755 "$BIN_FILE"
 else
